f412b5a1f0f51ae8656b80507c5eda2a1cbd4c26
jeanandersonhs <jeanhugo2016@gmail.com>
2026-05-24 19:55:17 -0300

    feat: implement user selection component and update App to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
-import './App.css'
-
+import {useState} from 'react';
+import {SelectUser} from './components/selectUser';
 
 function App() {
+
+
+  const users = [
+    { id: 1, name: 'Usuário 1' },
+    { id: 2, name: 'Usuário 2' },
+    { id: 3, name: 'Usuário 3' },
+  ];
 
   return (
     <>
       <div>
-        Teste
+        <SelectUser 
+          users={users}
+          
+          />
       </div>
     </>
   )
