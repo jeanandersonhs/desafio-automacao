92d3b6269d9ba761e402480cad0f864701277e89
jeanandersonhs <jeanhugo2016@gmail.com>
2026-05-24 22:08:28 -0300

    feat: add PostItem component to fetch and display comments, and refactor metrics calculation in FilterResult

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,32 @@
 import { useState } from 'react';
 
 import SelectUser from './components/selectUser';
+import FilterResult from './components/filterResult';
 import { useUserQuery } from './hooks/queries/useUserQuery';
 import { usePostQuery } from './hooks/queries/usePostQuery';
 
 function App() {
   const [selectedUserId, setSelectedUserId] = useState(null);
   const { data: users = [], isLoading : loadingUsers, isError : erroUser } = useUserQuery();
-  const { data: posts = [] } = usePostQuery(selectedUserId, {
-    skip: !selectedUserId,});
+
+  const { data: posts = [] } = usePostQuery(selectedUserId);
+  
 
   if (loadingUsers) return <p>Carregando usuários...</p>
   if (erroUser) return <p>Erro ao carregar usuários</p>
 
-  const onSelect = (e) => {
-    const userId = e.target.value;
+  const onSelect =  (e) => {
+    const userId = Number(e.target.value);
+
     setSelectedUserId(userId);
     console.log('Usuário selecionado:', userId);
   };
+
   return (
     <>
       <div>
         <SelectUser users={users} onSelect={onSelect}/>
-        <filterResult users={users} idUserSelected={selectedUserId} posts={posts}/>
+        {<FilterResult users={users} idUserSelected={selectedUserId} posts={posts}/>}
       </div>
     </>
   )
