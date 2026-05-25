40528d7288f93619ac4aaa5baec011e1df4701b8
jeanandersonhs <jeanhugo2016@gmail.com>
2026-05-25 16:17:50 -0300

    feat: query function to get comments for each post

--- src/hooks/queries/useComentsQuery.js
+++ src/hooks/queries/useComentsQuery.js
@@ -11,3 +11,13 @@ export function useCommentsQuery(postId) {
         enabled: !!postId,
     })
 }
+
+
+export function usePostsWithComments(posts) {
+
+    return useQuery({
+        queryKey: ['posts-with-comments', posts?.map(p => p.id) ||[]], // Chave única baseada nos IDs dos posts
+        queryFn: () => apiService.getPostWithComments(posts),
+        enabled: !!posts && posts.length>0, // A query só é executada se post for válido (não nulo ou indefinido)
+    })
+}
