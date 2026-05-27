99f62da740cc2170be6b6ef221906836108b593b
jeanandersonhs <jeanhugo2016@gmail.com>
2026-05-27 16:39:56 -0300

    fix: loadding spinner

--- src/components/ui/loading.jsx
+++ src/components/ui/loading.jsx
@@ -1,6 +1,6 @@
 import { Spinner } from "./spinner";
 
-export default function Loading() {
+export function Loading() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-background/50 backdrop-blur-sm z-50 fixed inset-0">
       <Spinner size="lg" />
